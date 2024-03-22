8e2fcd1ce18a75664ab25e0684f2f403f83e0904
08786218654 <leandrosena@lampp-it.com.br>
2024-03-22 11:31:13 -0300

    feat: fim do exercicio 2

--- desafio-2.py
+++ desafio-2.py
@@ -1,13 +1,13 @@
 # Regra da sequência de Fibonacci: Primeiro e segundo termo igual a 1
-def geradorSequencia(n):
-    fibonacciSequencia = [0, 1]
-    while fibonacciSequencia[-1] <= n:
+def geradorSequencia(numero):
+    fibonacciSequencia = [1, 1]
+    while fibonacciSequencia[-1] <= numero:
         fibonacciSequencia.append(fibonacciSequencia[-1] + fibonacciSequencia[-2])
     return fibonacciSequencia[:-1]
 
-def verificaSePertence(n):
-    fibonacciSequencia = geradorSequencia(n)
-    if n in fibonacciSequencia:
+def verificaSePertence(numero):
+    fibonacciSequencia = geradorSequencia(numero)
+    if numero in fibonacciSequencia:
         return True
     else:
         return False
@@ -15,12 +15,20 @@ def verificaSePertence(n):
 def verificaPrimeirosValores(numero):
     if numero == 1:
         return True
+def verificaPosicao(numero):
+    fibonacciSequencia = geradorSequencia(numero)
+    return fibonacciSequencia.index(numero)
 
+
+
+#inicio da execucao, sem main
 numero = int(input("Entre com um número: "))
 
 if verificaPrimeirosValores(numero):
-    print("\nValor digitado pode ser o primeiro ou segundo termo da sequência.")
+    print(f"Valor digitado pode ser o primeiro ou segundo termo da sequência.")
 if verificaSePertence(numero):
     print(f"O número {numero} pertence à sequência de Fibonacci.")
+    print(f"O número {numero} está na posição {verificaPosicao(numero)}")
+
 else:
     print(f"O número {numero} não pertence à sequência de Fibonacci.")
